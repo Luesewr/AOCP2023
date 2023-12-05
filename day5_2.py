da664f7221eca4dcffeb8878441bca9d8586eaf1
Laurens de Swart <L.j.p.deSwart@student.tudelft.nl>
2023-12-05 17:17:53 +0100

    Removed unnecessary things

--- day5_2.py
+++ day5_2.py
@@ -2,10 +2,8 @@ f = open("inputs/day5.txt")
 e = f.read().split("\n\n")
 seed_pairs = [int(i) for i in e[0].split(": ")[1].split(" ")]
 it = iter(seed_pairs)
-seeds = sorted(list(zip(it, it)), key=lambda x: x[0])
-maps = e[1:]
-maps = [sorted([[int(k) for k in j.split(" ")] for j in i.split("\n")[1:]], key=lambda x: x[1]) for i in maps]
-locations = []
+seeds = zip(it, it)
+maps = [sorted([[int(k) for k in j.split(" ")] for j in i.split("\n")[1:]], key=lambda x: x[1]) for i in e[1:]]
 
 current_values = seeds
 for map_n in maps:
@@ -26,15 +24,12 @@ for map_n in maps:
                     next_values.append((last_highest, value_length))
                     last_highest = s_r
                     remaining_length -= value_length
-            if last_highest <= s_r < end_of_value:
+            if s_r < end_of_value:
                 value_length = min(length, remaining_length)
                 if value_length > 0:
                     next_values.append((last_highest + d_r - original_s_r, value_length))
                     last_highest = s_r + value_length
                     remaining_length -= value_length
-
-        if remaining_length > 0:
-            next_values.append((last_highest, remaining_length))
     current_values = next_values
 
 print(sorted(current_values, key=lambda x: x[0])[0][0])
