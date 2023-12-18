acbd740c61993b5a19b3965f53a647e82437ad6d
Laurens de Swart <L.j.p.deSwart@student.tudelft.nl>
2023-12-19 00:05:07 +0100

    Optimised and fixed day 17

--- day17_1.py
+++ day17_1.py
@@ -1,4 +1,4 @@
-from queue import PriorityQueue
+import heapq
 
 f = open("inputs/day17.txt")
 lines = f.read().split("\n")
@@ -14,7 +14,7 @@ class QueueItem:
         self.straight_counter = straight_counter
 
     def __lt__(self, other):
-        return self.heat_loss < other.heat_loss
+        return self.heat_loss - self.location[0] - self.location[1] < other.heat_loss - other.location[0] - other.location[1]
 
 
 def apply_direction(x, y, d):
@@ -32,25 +32,22 @@ def check_bounds(x, y, width, height):
     return 0 <= x < width and 0 <= y < height
 
 
-pq = PriorityQueue()
-pq.put(QueueItem((1, 0, 0), heat_values[0][1], 1))
-pq.put(QueueItem((0, 1, 1), heat_values[1][0], 1))
+heap = []
+heapq.heappush(heap, QueueItem((1, 0, 0), heat_values[0][1], 1))
+heapq.heappush(heap, QueueItem((0, 1, 1), heat_values[1][0], 1))
 
 best_value = sum([sum(values) for values in heat_values])
 
-visited = {}
+visited = set()
 
-while not pq.empty():
-    pq_item = pq.get()
+while heap:
+    pq_item = heapq.heappop(heap)
     x, y, d = pq_item.location
-    info = (x, y, d)
-    if info in visited and pq_item.straight_counter >= visited[info]:
+    info = (x, y, d, pq_item.straight_counter)
+    if info in visited:
         continue
 
-    visited[info] = pq_item.straight_counter
-
-    if best_value < pq_item.heat_loss:
-        break
+    visited.add(info)
 
     if x == width - 1 and y == height - 1:
         best_value = pq_item.heat_loss
@@ -61,7 +58,9 @@ while not pq.empty():
             continue
         direction = (d + i + 4) % 4
         new_x, new_y = apply_direction(x, y, direction)
-        if check_bounds(new_x, new_y, width, height):
-            pq.put(QueueItem((new_x, new_y, direction), pq_item.heat_loss + heat_values[new_y][new_x], pq_item.straight_counter + 1 if i == 0 else 1))
+        new_straight_counter = pq_item.straight_counter * (i == 0) + 1
+        new_info = (new_x, new_y, direction, new_straight_counter)
+        if new_info not in visited and check_bounds(new_x, new_y, width, height):
+            heapq.heappush(heap, QueueItem((new_x, new_y, direction), pq_item.heat_loss + heat_values[new_y][new_x], pq_item.straight_counter + 1 if i == 0 else 1))
 
 print(best_value)
